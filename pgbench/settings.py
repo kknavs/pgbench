c8122347a8d0aee90098264777cdbfb9da083aa3
domc <domc987@gmail.com>
2013-03-28 21:03:03 +0100

    db model-zacetna verzija

--- pgbench/settings.py
+++ pgbench/settings.py
@@ -6,6 +6,7 @@ TEMPLATE_DEBUG = DEBUG
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
     ('Jon', 'jon.leviathan@gmail.com'),
+    ('Domc', 'domc987@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -13,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': 'D:/GitFolder/pgbench/db/sqlite3.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -123,6 +124,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'pgbench',
+    'submitted_measurement',
 )
 
 # A sample logging configuration. The only tangible logging
