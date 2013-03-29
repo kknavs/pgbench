43b3cba450656599749d27f370ce4af20c1606e2
JonNatanael <jon.leviathan@gmail.com>
2013-03-29 18:43:32 +0100

    Changes to forms and urls

--- pgbench/urls.py
+++ pgbench/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from pgbench.frontend import views as frontend_views
+from pgbench.users import views as user_views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,7 +12,7 @@ urlpatterns = patterns('pgbench.frontend.views',
 )
 
 urlpatterns += patterns('pgbench.users.views',
-                        ('^login$', 'login'),
+                        url('^login$', user_views.RegisterView.as_view(), name='registration'),
                         ('^register$', 'register'),
 )
 
