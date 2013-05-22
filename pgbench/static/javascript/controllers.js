c3325766d8feb5c735085cf6609967aa272ea006
JonNatanael <jon.leviathan@gmail.com>
2013-05-22 18:54:39 +0200

    Added insertData command.

--- pgbench/static/javascript/controllers.js
+++ pgbench/static/javascript/controllers.js
@@ -15,6 +15,7 @@ function pgbenchCtrlS($scope, SearchM) {
                 $scope.searchM=data;
                 //alert(data['objects'].length);
                 var s='<tr><th>Id</th> <th>Title</th></tr>' //other poss.?
+                console.log(data['objects'].length);
                 for (var i = 0; i < data['objects'].length; i++) {
                  s+="<tr> <td>" + data['objects'][i]['id'] + "</td><td>" + data['objects'][i]['title'] + "</td></tr>"
                 }
